de7a072088310bd6e11db2f7df45244ef41359e8
Daeshaun Willis <ddwillis59@gmail.com>
2023-01-26 18:14:47 -0500

    fixed everything to deploy locally

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,32 @@
 import React from "react";
-import Heading from "./Heading";
-import Footer from "./Footer";
-import Note from "./Note";
 
-function App(){
-    return(
-        <div>
-            <Heading/>
-            <Note/>
-            <Footer/>
-        </div>
-        
-    )
+function App() {
+  const date = new Date().toLocaleTimeString("en-US", {
+    hour12: false,
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric"
+  });
+
+  const [time, setTime] = React.useState(date);
+
+  function updateTime() {
+    const newTime = new Date().toLocaleTimeString("en-US", {
+      hour12: false,
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric"
+    });
+    setTime(newTime);
+  }
+
+  setInterval(updateTime, 1000);
+  return (
+    <div className="container">
+      <h1>{time}</h1>
+      {/* <button onClick={updateTime}>Get Time</button> */}
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Heading.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Heading() {
-    return (
-        <header className="header"><h1>Keeper App</h1></header>
-    )
-};
-
-export default Heading;
