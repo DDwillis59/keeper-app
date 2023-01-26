157f34d52d589eb2c5958b8d4a398fab3c374dca
Daeshaun Willis <ddwillis59@gmail.com>
2023-01-26 18:21:39 -0500

    fixed merge from other project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
+
 import Heading from "./Heading";
 import Footer from "./Footer";
 import Note from "./Note";
@@ -16,36 +16,5 @@ export default function App(){
     )
 }
 
-=======
 
-function App() {
-  const date = new Date().toLocaleTimeString("en-US", {
-    hour12: false,
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric"
-  });
 
-  const [time, setTime] = React.useState(date);
-
-  function updateTime() {
-    const newTime = new Date().toLocaleTimeString("en-US", {
-      hour12: false,
-      hour: "numeric",
-      minute: "numeric",
-      second: "numeric"
-    });
-    setTime(newTime);
-  }
-
-  setInterval(updateTime, 1000);
-  return (
-    <div className="container">
-      <h1>{time}</h1>
-      {/* <button onClick={updateTime}>Get Time</button> */}
-    </div>
-  );
-}
-
-export default App;
->>>>>>> 3b5a95ff8fb4b89ca02097cd2e0eb543d812ae73
